a185ba9141b7e6421ce888302b844a79d33e8c3e
Davis Guest <davisdevice@gmail.com>
2024-05-31 17:27:01 -0400

    updated formatting

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import math
-import tkinter as tk
+import tkinter
 from tkinter import ttk
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 
@@ -11,13 +11,12 @@ import numpy as np
 
 units_NMI   = ["NMI", "m", "mi", "ft"]
 units_dBW   = ["dBW", "dBm", "W", "mW"]
-units_rcs  = ["NMI\u00B2", "m\u00B2", "mi\u00B2", "ft\u00B2"]
+units_rcs  = ["m\u00B2", "cm\u00B2", "ft\u00B2", "in\u00B2"]
 units_GHz  = ["GHz", "MHz", "kHz", "Hz"]
 
 
 ##################################### Calculations ######################################
 """
-
         Pt * Gt * Gr * λ^2 * σ
 Pr = ---------------------------
             (4π)^3 * R^4
@@ -85,7 +84,15 @@ def calculate_and_plot():
     freq  = convert_to_Hz(float(freq_entry.get()), freq_unit.get())
     rcs    = convert_to_NMI2(float(rcs_entry.get()), rcs_unit.get())
     range  = convert_to_NMI(float(range_entry.get()), range_unit.get())
-    pwr_r = radar_range_equation(pwr_t, gain_t, gain_r, freq, rcs, range)
+
+    range_values = np.linspace(1, 200, 400)
+    pwr_r_values = []
+
+    for r in range_values:
+        print(r)
+        pwr_r = radar_range_equation(pwr_t, gain_t, gain_r, freq, rcs, r)
+        print(f"range=  {r}       {pwr_r}")
+        pwr_r_values.append(pwr_r)
 
     print("AFTER:")
     print("------")
@@ -97,14 +104,17 @@ def calculate_and_plot():
     print(f"range    :  {range}")
     print("------------------------------")
     print(f"pwr_r      :  {pwr_r}")
+
+    
+    
     
     fig.clear()
     ax = fig.add_subplot(111)
-    ax.plot(range, 10 * np.log10(pwr_r), label="Received Power")
+    ax.plot(range_values, 10 * np.log10(pwr_r_values), label="Received Power")
     # ax.plot(range, 10 * np.log10(pwr_n), label="Noise Jammer")
     # ax.plot(range, 10 * np.log10(P_range_j), label="Repeater Jammer")
-    ax.set_xlabel(f"Range ({range_unit.get()})")
-    ax.set_ylabel('Received Power (dBW)')
+    ax.set_xlabel(f"Range ({plot_x_unit.get()})")
+    ax.set_ylabel(f"Received Power {plot_y_unit.get()}")
     ax.legend()
     canvas.draw()
 
@@ -112,19 +122,19 @@ def calculate_and_plot():
 ###################################### GUI Setup ########################################
 
 
-root = tk.Tk()
+root = tkinter.Tk()
 root.title("Radar Range Equation Calculator")
 default_font = ('Product Sans', 13)
 
 
 # Power Transmitted
-tk.Label(root,
+tkinter.Label(root,
          text="Pt : Power Transmitted",
          font=default_font
-        ).grid(row=0, column=0)
-pwr_t_entry = tk.Entry(root)
-pwr_t_entry.grid(row=0, column=1)
-pwr_t_unit = tk.StringVar()
+        ).grid(row=0, column=0, sticky="w", padx=10, pady=10)
+pwr_t_entry = tkinter.Entry(root)
+pwr_t_entry.grid(row=0, column=1, pady=10)
+pwr_t_unit = tkinter.StringVar()
 pwr_t_unit.set("dBW")
 pwr_t_unit_menu = ttk.Combobox(root,
                                textvariable=pwr_t_unit,
@@ -133,58 +143,57 @@ pwr_t_unit_menu = ttk.Combobox(root,
                                state="readonly",
                                width=6
                                )
-pwr_t_unit_menu.grid(row=0, column=2)
+pwr_t_unit_menu.grid(row=0, column=2, pady=10)
 
 ###############################################
 
 # Gain Transmitted
-tk.Label(root,
+tkinter.Label(root,
          text="Gt : Gain Transmitted",
          font=default_font
-        ).grid(row=1, column=0)
-gain_t_entry = tk.Entry(root)
-gain_t_entry.grid(row=1, column=1)
+        ).grid(row=1, column=0, sticky="w", padx=10, pady=10)
+gain_t_entry = tkinter.Entry(root)
+gain_t_entry.grid(row=1, column=1, pady=10)
 
 ###############################################
 
 # Gain Received
-tk.Label(root,
+tkinter.Label(root,
          text="Gr : Gain Received",
          font=default_font
-        ).grid(row=2, column=0)
-gain_r_entry = tk.Entry(root)
-gain_r_entry.grid(row=2, column=1)
+        ).grid(row=2, column=0, sticky="w", padx=10, pady=10)
+gain_r_entry = tkinter.Entry(root)
+gain_r_entry.grid(row=2, column=1, pady=10)
 
 ###############################################
 
 # Frequency
-tk.Label(root,
-         text="\u03BB : Frequency",
+tkinter.Label(root,
+         text="f : Frequency",
          font=default_font
-        ).grid(row=3, column=0)
-freq_entry = tk.Entry(root)
-freq_entry.grid(row=3, column=1)
-freq_unit = tk.StringVar()
+        ).grid(row=3, column=0, sticky="w", padx=10, pady=10)
+freq_entry = tkinter.Entry(root)
+freq_entry.grid(row=3, column=1, pady=10)
+freq_unit = tkinter.StringVar()
 freq_unit.set("GHz")
 freq_unit_menu = ttk.Combobox(root,
                               textvariable=freq_unit,
                               values=units_GHz,
                               font=default_font,
                               state="readonly",
-                              width=6
-                              )
-freq_unit_menu.grid(row=3, column=2)
+                              width=6)
+freq_unit_menu.grid(row=3, column=2, pady=10)
 
 ###############################################
 
 # RCS
-tk.Label(root,
+tkinter.Label(root,
          text="\u03C3 : Radar Cross Secion",
          font=default_font
-        ).grid(row=4, column=0)
-rcs_entry = tk.Entry(root)
-rcs_entry.grid(row=4, column=1)
-rcs_unit = tk.StringVar()
+        ).grid(row=4, column=0, sticky="w", padx=10, pady=10)
+rcs_entry = tkinter.Entry(root)
+rcs_entry.grid(row=4, column=1, pady=10)
+rcs_unit = tkinter.StringVar()
 rcs_unit.set("m\u00B2")
 rcs_unit_menu = ttk.Combobox(root,
                              textvariable=rcs_unit,
@@ -193,35 +202,79 @@ rcs_unit_menu = ttk.Combobox(root,
                              state="readonly",
                              width=6
                              )
-rcs_unit_menu.grid(row=4, column=2)
+rcs_unit_menu.grid(row=4, column=2, pady=10)
 
 ###############################################
 
 # Range
-tk.Label(root,
+tkinter.Label(root,
          text="R : Range",
          font=default_font
-        ).grid(row=5, column=0)
-range_entry = tk.Entry(root)
-range_entry.grid(row=5, column=1)
-range_unit = tk.StringVar()
+        ).grid(row=5, column=0, sticky="w", padx=10, pady=10)
+range_entry = tkinter.Entry(root)
+range_entry.grid(row=5, column=1, pady=10)
+range_unit = tkinter.StringVar()
 range_unit.set("NMI")
 range_unit_menu = ttk.Combobox(root,
                                textvariable=range_unit,
                                values=units_NMI,
                                font=default_font,
                                state="readonly",
-                               width=6)
-range_unit_menu.grid(row=5, column=2)
+                               width=6
+                              )
+range_unit_menu.grid(row=5, column=2, pady=10)
 
-# Plot button
-btn_plot = tk.Button(root, text="Plot", command=calculate_and_plot, font=default_font)
-btn_plot.grid(row=9, columnspan=2)
+tkinter.Label(root, text="").grid(row=6, columnspan=5)
 
 # Matplotlib figure setup
-fig = plt.Figure()
+fig = pyplot.Figure()
 canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.get_tk_widget().grid(row=10, columnspan=5)
+canvas.get_tk_widget().grid(row=8, columnspan=5)
+
+ax = fig.add_subplot(111)
+ax.plot(190, 1, label="Received Power")
+ax.plot(190, 1, label="Noise Jammer")
+ax.plot(190, 1, label="Repeater Jammer")
+ax.legend()
+
+canvas.draw()
+
+# Plot button
+btn_plot = tkinter.Button(root, text="Plot", command=calculate_and_plot, font=default_font)
+btn_plot.grid(row=5, column=4, sticky="w")
+
+plot_x_unit = tkinter.StringVar()
+plot_x_unit.set("NMI")
+tkinter.Label(root,
+              text="   x Unit",
+              font=default_font
+              ).grid(row=3, column=3, sticky="w")
+
+plot_x_unit_menu = ttk.Combobox(root,
+                                textvariable=plot_x_unit,
+                                values=units_NMI,
+                                font=default_font,
+                                state="readonly",
+                                width=6)
+plot_x_unit_menu.grid(row=3, column=4, sticky="w")
+
+plot_y_unit = tkinter.StringVar()
+plot_y_unit.set("dBW")
+tkinter.Label(root,
+         text="   y Unit",
+         font=default_font
+        ).grid(row=4, column=3, sticky="w")
+
+plot_y_unit_menu = ttk.Combobox(root,
+                               textvariable=plot_y_unit,
+                               values=units_dBW,
+                               font=default_font,
+                               state="readonly",
+                               width=6)
+plot_y_unit_menu.grid(row=4, column=4, sticky="w")
+
+ax.set_xlabel(f"Range ({plot_x_unit.get()})")
+ax.set_ylabel(f"Received Power {plot_y_unit.get()}")
 
 root.mainloop()
 
