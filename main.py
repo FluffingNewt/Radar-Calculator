43e97ad93984cbff8776cda92f92f3686ec6a2bc
Davis Guest <davisdevice@gmail.com>
2024-06-26 10:02:49 -0400

    replaced old logarithmic tab with test file

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from tkinter          import ttk
 from tabs.linear_tab  import Tab1
 from tabs.log_tab     import Tab2
 from tabs.doppler_tab import Tab3
-from tabs.log_tab_test import Tab4
 
 root = tkinter.Tk()
 root.geometry("915x1010")
@@ -21,13 +20,11 @@ notebook.pack(expand=True)
 
 tab1 = Tab1(notebook)
 tab2 = Tab2(notebook)
-tab3 = Tab3(notebook)
-tab4 = Tab4(notebook)
+tab3 = Tab3(notebook)\
 
 notebook.add(tab1, text="Linear")
 notebook.add(tab2, text="Logarithmic")
 notebook.add(tab3, text="Doppler")
-notebook.add(tab4, text="Log-Test")
 
 # Run the application
 root.mainloop()
